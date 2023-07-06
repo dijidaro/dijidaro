3db0a73ad3dc39ab24456d2f12510488733ef440
otivos <vostowoko@gmail.com>
2023-07-06 21:40:06 +0300

    Refactored the app structure for heroku

--- app/app.py
+++ app/app.py
@@ -1,6 +1,12 @@
-from config import create_app
+import os
+from flask import Flask, render_template
 
-app = create_app()
+app = Flask(__name__, instance_relative_config=True)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
