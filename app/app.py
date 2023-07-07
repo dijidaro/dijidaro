9ab99b238d7e9653856439502af46183f8e1ed43
otivos <vostowoko@gmail.com>
2023-07-07 17:08:06 +0300

    removed modules

--- app/app.py
+++ app/app.py
@@ -1,7 +1,19 @@
-import os
-from config import create_app
+# import os
+# from config import create_app
 
-app = create_app()
+# app = create_app()
+
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port, debug=True)
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
