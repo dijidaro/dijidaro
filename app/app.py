bdcc2de8881ee9f32603bb88414eb830d9460af3
otivos <vostowoko@gmail.com>
2023-07-13 21:21:22 +0300

    Added postgresql connection string

--- app/app.py
+++ app/app.py
@@ -1,8 +1,18 @@
 import os
 from config import create_app
+from models import db
+from flask_migrate import Migrate
 
 app = create_app()
+
+# bootstrap database migrate commands
+db.init_app(app)
+migrate = Migrate(app, db)
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
+
