a604421e18bb0026bc64d95ac052cf24e96355de
otivos <vostowoko@gmail.com>
2023-07-14 09:52:58 +0300

    changed database url

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
     app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
 
     import dashboard
     app.register_blueprint(dashboard.bp)
