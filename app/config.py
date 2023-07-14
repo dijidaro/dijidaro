33a65e6005590e418b5eefe83e2ed7d9a97ee878
otivos <vostowoko@gmail.com>
2023-07-14 21:37:03 +0300

    LAST MILE

--- app/config.py
+++ app/config.py
@@ -1,11 +1,12 @@
-# import os
+import os
 from flask import Flask
 
 # Create and configure app.
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
-    #app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     import dashboard
     app.register_blueprint(dashboard.bp)
