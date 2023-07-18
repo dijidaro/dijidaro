429f878a46903864273ba280c7aecd72bfdc8230
otivos <vostowoko@gmail.com>
2023-07-18 21:34:37 +0300

    Migration pt2

--- app/auth.py
+++ app/auth.py
@@ -9,6 +9,7 @@ bp = Blueprint("auth", __name__, url_prefix="/auth")
 @bp.route("/student_registration", methods=("GET", "POST"))
 def register_student():
     form = StudentRegistrationForm()
+
     if form.validate_on_submit():
         try:
             student = Student(first_name=form.firstname.data, last_name=form.lastname.data, username=form.username.data, email=form.email.data, 
@@ -16,15 +17,22 @@ def register_student():
             db.session.add(student)
             db.session.commit()
         except IntegrityError as e:
-            error = "Request failed! You either entered an already existing username and/or email."
+            error = f"Request failed! You either entered an already existing username and/or email. {e}"
             db.session.rollback()
             print(error)
         else:
-            return redirect(url_for("auth.login"))
-
+            return redirect(url_for("auth.login_student"))
+        
     return render_template("auth/register.html", form=form)
 
 @bp.route("student_login", methods=("GET", "POST"))
 def login_student():
     form = StudentLoginForm()
     return render_template("auth/login.html", form=form)
+
+@bp.route("/delete/<int:student_id>", methods=("POST", "GET"))
+def delete_student(student_id):
+    student = Student.query.get(student_id)
+    db.session.delete(student)
+    db.session.commit()
+    return redirect(url_for("auth.login_student"))
