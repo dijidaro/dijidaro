b98c01a38456926265a6b0be11029ab7bdb5bb5b
owokosteve <stephenowoko2@gmail.com>
2023-08-25 14:06:18 +0300

    created tables for resources, subjects and schools

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from models import User
+from models import School, User, Subject
 from forms import DeleteForm
 
 bp = Blueprint("admin", __name__)
@@ -8,7 +8,9 @@ bp = Blueprint("admin", __name__)
 def index():
     form = DeleteForm()
     users = User().query.all()
+    subjects = Subject().query.all()
+    schools = School().query.all()
 
     if form.validate_on_submit():
         return render_template("admin.html", form=form)
-    return render_template("admin.html", users=users, form=form )
+    return render_template("admin.html", users=users, subjects=subjects, schools=schools, form=form )
