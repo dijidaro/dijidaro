d64dc869f5bf0ab27badfc7d75ad820bf0cfc6c1
owokosteve <stephenowoko2@gmail.com>
2024-11-13 17:02:35 +0300

    Update explore route

---
+++ app/routes/explore.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+explore_bp = Blueprint("explore", __name__)
+
+@explore_bp.route("/explore", methods=["GET"])
+def explore_resources():
+    return render_template("explore.html")
