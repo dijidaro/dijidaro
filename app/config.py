d9da8af488c7ff685d6205e905f359acf6386ed3
owokosteve <stephenowoko2@gmail.com>
2024-11-22 18:47:55 +0300

    Fix typo loggin to logging

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 from error_handlers import init_error_handlers
-from loggin_config import configure_logging
+from app.logging_config import configure_logging
 
 csrf=CSRFProtect()
 
